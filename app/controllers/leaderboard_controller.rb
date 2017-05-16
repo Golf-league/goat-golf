810d6d4074a6443a5a1a7743b15342090aeef981
Chris Ward <chris.ward@alliants.com>
2017-05-16 14:33:45 +0100

    orders leaderboard

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -1,5 +1,5 @@
 class LeaderboardController < ApplicationController
   def leaderboard
-    @users = User.all.sort_by{ |user| user.total_score }
+    @users = User.all.sort_by{ |user| user.total_score }.reverse
   end
 end
