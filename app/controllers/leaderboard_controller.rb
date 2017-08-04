05b82608b4166a243a844c8f87f050c593e13b13
Chris Ward <chris.ward@alliants.com>
2017-08-04 13:33:19 +0100

    adds this year view

--- app/controllers/leaderboard_controller.rb
+++ app/controllers/leaderboard_controller.rb
@@ -2,4 +2,8 @@ class LeaderboardController < ApplicationController
   def leaderboard
     @users = User.all.sort_by{ |user| user.total_score }.reverse
   end
+
+  def this_year
+    @users = User.all.sort_by{ |user| user.total_score }.reverse
+  end
 end
